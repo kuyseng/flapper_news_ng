b52e7c3ef231b65bef8b03fb3cd85c73ee7ce942
kuyseng <kuyseng@gmail.com>
2016-09-18 06:19:45 +0700

    rewrite by memory

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-angular.module('flapperNews', [])
-  .controller('MainCtrl', [
-    '$scope',
-    function($scope) {
-      $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 1},
-        {title: 'post 4', upvotes: 3},
-        {title: 'post 5', upvotes: 6},
-        {title: 'post 6', upvotes: 7},
-        {title: 'post 7', upvotes: 0}
-      ];
+angular.module('FlapperNews', [])
+  .controller('MainCtrl', ['$scope', function($scope) {
+    $scope.posts = [
+      { title: 'post 1', upvotes: 5},
+      { title: 'post 2', upvotes: 2},
+      { title: 'post 3', upvotes: 1},
+      { title: 'post 4', upvotes: 3},
+      { title: 'post 5', upvotes: 0}
+    ]
+    $scope.addPost = addPost;
+    $scope.increaseUpVotes = increaseUpVotes;
 
-      $scope.addPost= function() {
-        if(!$scope.title || $scope==='') return;
-        $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
-        $scope.title ='';
-        $scope.link ='';
-      };
-      $scope.increaseUpvotes = function(post) {
-        post.upvotes += 1;
-      }
+    function addPost() {
+      if(!$scope.title || $scope.title==='') return;
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.title = '';
+      $scope.link = '';
     }
-  ])
 
+    function increaseUpVotes(post) {
+      post.upvotes += 1;
+    }
+  }])
