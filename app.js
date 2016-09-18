15eded171e8bb24d4fba09b5a35bf0286038a631
kuyseng <kuyseng@gmail.com>
2016-09-18 08:09:56 +0700

    use ui-routes and add link to page detail to see its comments

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      })
+
+    $urlRouterProvider.otherwise('home');
+  }])
   .controller('MainCtrl', ['$scope', 'postService', function($scope, postService) {
     $scope.posts = postService.posts;
     $scope.addPost = addPost;
@@ -7,7 +22,15 @@ angular.module('flapperNews', [])
 
     function addPost() {
       if(!$scope.title || $scope.title==='') return;
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
       $scope.title = '';
       $scope.link = '';
     }
@@ -20,10 +43,16 @@ angular.module('flapperNews', [])
       $scope.posts.splice(index, 1);
     }
   }])
+  .controller('PostsCtrl', ['$scope', '$stateParams', 'postService', function($scope, $stateParams, postService) {
+    index = parseInt($stateParams.id)
+    $scope.post = postService.posts[index];
+  }])
   .factory('postService', [function() {
     var obj = {
-      posts: []
+      posts: [{title: 'test', upvotes: 1}, {title: 'test again',  upvotes: 0}]
     }
 
     return obj;
   }])
+
+
